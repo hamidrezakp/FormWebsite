b6404a644d7bbad90903c23b55aae40fbcc7bfe7
Hamid R. K. Pisghadam <hamidrkp@riseup.net>
2022-02-17 21:37:14 +0330

    fix headers in cors

--- src/website/cors.rs
+++ src/website/cors.rs
@@ -7,7 +7,7 @@ pub fn cors_fairing() -> Cors {
             .into_iter()
             .map(From::from)
             .collect(),
-        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Platform-version"]),
+        allowed_headers: AllowedHeaders::All,
         allow_credentials: true,
         ..Default::default()
     }
